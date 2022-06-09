7d9b6cd8f9055644c66cd77e63d603d1c8c0908b
SandraGii <lsgaseitsiwe@gmail.com>
2022-06-09 12:26:05 -0700

    0-main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-int n;
+int i;
 srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
+i = rand() - RAND_MAX / 2;
+if (i > 0)
+printf("%d is positive\n", i);
 else if
-(n == 0)
-printf("%d is zero\n", n);
+(i == 0)
+printf("%d is zero\n", i);
 else
-printf("%d is negative\n", n);
+printf("%d is negative\n", i);
 return (0);
 }
